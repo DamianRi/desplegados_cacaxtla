d1894362abfd677d2efce8a4c0d375692749e202
DamianRi <dams_rive@hotmail.com>
2022-02-21 03:09:19 -0600

    Onclick sobre los menú items
    Para ampliar a detalle el menú seleccionado.

--- src/interactions.js
+++ src/interactions.js
@@ -33,3 +33,59 @@ function manageMuralSectionLong() {
         manageMuralSectionShort();
     }
 }
+
+/**
+ * Adds/removes the class 'main-menu__item--open' to the
+ * clicked menu item
+ *
+ * @param {*} currentItem: menu item clicked
+ * @returns
+ */
+function manageMenuItem(currentItem) {
+    if (!currentItem) return
+    // close all items previusly opened
+    let openedItems = getOpenedItems();
+    if (openedItems.length) {
+        closeMenuItems(openedItems, currentItem);
+    }
+
+    let classListItem = currentItem.classList;
+    if (classListItem.contains("main-menu__item--open")) {
+        classListItem.remove("main-menu__item--open");
+    } else {
+        classListItem.add("main-menu__item--open");
+    }
+}
+
+/**
+ * Gets an HTMLCollection with the elements that have the
+ * 'main-menu__item--open' class. All this items should be
+ * only menu items.
+ *
+ * @returns {HTMLCollection}
+ */
+function getOpenedItems() {
+    let items = document.getElementsByClassName("main-menu__item--open");
+    return items;
+}
+
+/**
+ * Removes the class 'main-menu__item--open' for each item in the list,
+ * except for the 'exceptItem'.
+ *
+ * @param {HTMLCollection} list: of menu items opened
+ * @param {object} exceptItem: item that will not remove the class
+ * @returns
+ */
+function closeMenuItems(list, exceptItem) {
+    for (const itemOpened of list) {
+        if (itemOpened.id !== exceptItem.id)
+            itemOpened.classList.remove("main-menu__item--open");
+    }
+}
+function redirectToMurals() {
+    window.location.href = "/views/murales.html";
+}
+function redirectToHome() {
+    window.location.href = "/index.html";
+}
