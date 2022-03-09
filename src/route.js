701a7ce77aa81d16dda85d922700b6e7f6fa8a2d
DamianRi <dams_rive@hotmail.com>
2022-03-08 22:48:41 -0600

    Fix de urls para redireccionamiento

--- src/route.js
+++ src/route.js
@@ -2,7 +2,7 @@
 function redirectToMurals(section) {
     let params = new URLSearchParams()
     params.append("section", section)
-    window.location.href = "/views/murales.html?" + params.toString();
+    window.location.href = "views/murales.html?" + params.toString();
 }
 function redirectToHome() {
     window.location.href = "/index.html";
